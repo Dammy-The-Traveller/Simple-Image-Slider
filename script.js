5d008bb4ff884c878e089df98b75af8bd52e1574
Dammy-The-Traveller <adebesindamilare39@gmail.com>
2024-08-21 15:45:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// image slider
+
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 0;
+let interValidate = null;
+
+// initializeSlider();
+
+document.addEventListener('DOMContentLoaded',initializeSlider);
+
+function initializeSlider() {
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add('displaySlide');
+        interValidate= setInterval(nextSlide, 5000)
+    }
+}
+
+
+function showSlide(index) {
+    if(index >= slides.length){
+        slideIndex=0
+    }else if(index <0){
+
+        slideIndex=slides.length - 1;
+    }
+   slides.forEach(slide=>{
+    slide.classList.remove('displaySlide');
+  })
+    slides[slideIndex].classList.add('displaySlide');
+ }
+
+
+
+function prevslide(){
+    clearInterval(interValidate)
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+
+
+function nextSlide(){
+slideIndex++;
+showSlide(slideIndex);
+}
